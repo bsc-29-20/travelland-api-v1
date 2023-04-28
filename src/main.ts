2ed034bd995d4f8070ecc660adabab8fa4486576
Your Name <you@example.com>
2023-04-29 01:11:59 +0200

    adding a swagger recipe

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
 
-  // this will throw an error if there will be one
-  const app = await NestFactory.create(AppModule, { abortOnError: false });
+   // this will throw an error if there will be one
+   const app = await NestFactory.create(AppModule, { abortOnError: false });
 
+  const config = new DocumentBuilder()
+    .setTitle('Travelland')
+    .setDescription('The travelland API description')
+    .setVersion('1.0')
+    .addTag('')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+ 
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
