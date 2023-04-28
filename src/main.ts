97de0820c207957b490b752e390960cf374d99d6
Your Name <you@example.com>
2023-04-29 01:16:59 +0200

    adding a swagger recipe

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
     .setTitle('Travelland')
     .setDescription('The travelland API description')
     .setVersion('1.0')
-    .addTag('')
+    .addTag('users')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
